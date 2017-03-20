fa555ca0d3ed2e812c02e1bf8be3479641140d81
Luz <luzma_87@yahoo.com>
2017-03-20 08:21:57 -0500

    added especie list

--- lib/domains/especie.rb
+++ lib/domains/especie.rb
@@ -1,30 +1,55 @@
 require 'hanami/validations'
 
+# noinspection RubyTooManyInstanceVariablesInspection
 class Especie
   include Hanami::Validations
 
+  # rubocop:disable Metric/MethodLength
+  # rubocop:disable Metric/AbcSize
   def initialize(params)
     @nombre = params[:nombre]
+    @genero = params[:genero]
+    @familia = params[:familia]
     @id_tropicos = params[:id_tropicos]
+    @color1 = params[:color1]
+    @color2 = params[:color2]
+    @forma_vida1 = params[:forma_vida1]
+    @forma_vida2 = params[:forma_vida2]
     @descripcion_es = params[:descripcion_es]
     @descripcion_en = params[:descripcion_en]
     @distribucion_es = params[:distribucion_es]
     @distribucion_en = params[:distribucion_en]
+    @thumbnail = params[:thumbnail]
   end
 
   attr_accessor :nombre
+  attr_accessor :genero
+  attr_accessor :familia
   attr_accessor :id_tropicos
+  attr_accessor :color1
+  attr_accessor :color2
+  attr_accessor :forma_vida1
+  attr_accessor :forma_vida2
   attr_accessor :descripcion_es
   attr_accessor :descripcion_en
   attr_accessor :distribucion_es
   attr_accessor :distribucion_en
+  attr_accessor :thumbnail
+
+  def nombre_cientifico
+    "#{@genero} #{@nombre}"
+  end
+
+  def thumbnail_path
+    "encyclopedia/thumbnails/#{@thumbnail}"
+  end
 
   def eql?(other)
-    other && @nombre == other.nombre
+    other && nombre_cientifico == other.nombre_cientifico && familia == other.familia
   end
 
   def hash
-    @nombre.hash
+    [nombre_cientifico, @familia].hash
   end
 
   alias == eql?
--- lib/repos/especie_repo.rb
+++ lib/repos/especie_repo.rb
@@ -2,19 +2,39 @@ require_relative '../domains/especie'
 
 class EspecieRepository
   def initialize(connection)
-    @db = connection[:especie]
+    @db = connection
   end
 
+  # rubocop:disable Metric/MethodLength
   def find_all
-    # DB.from(:albums, :artists).where{{artists[:id]=>albums[:artist_id]}}
-    # SELECT * FROM albums, artists WHERE artists.id = albums.artist_id
-    # DB.fetch("SELECT name FROM users") do |row|
-    #   p row[:name]
-    # end
+    sql = 'select e. id,
+            e.nombre nombre,
+            g.nombre genero,
+            f.nombre familia,
+            e.id_tropicos,
+            c1.nombre color1,
+            c2.nombre color2,
+            v1.nombre forma_vida1,
+            v2.nombre forma_vida2,
+            e.descripcion_es,
+            e.descripcion_en,
+            e.distribucion_es,
+            e.distribucion_en,
+            e.thumbnail
+          from especie e
+            left join genero g on e.genero_id = g.id
+            left join familia f on g.familia_id = f.id
+            left join color c1 on e.color1_id = c1.id
+            left join color c2 on e.color2_id = c2.id
+            left join forma_vida v1 on e.forma_vida1_id = v1.id
+            left join forma_vida v2 on e.forma_vida2_id = v2.id
+          order by genero, nombre'
 
     especies = []
-    result = @db.order(:nombre)
-    result.each { |row| especies.push(Especie.new(row)) }
+    @db.fetch(sql) do |row|
+      especie_new = Especie.new(row)
+      especies.push(especie_new)
+    end
     especies
   end
 end
